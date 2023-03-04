5b1624249f8b7838b1f3d3ff3a49ed524920e253
nh <nzhagen@gmail.com>
2023-03-04 17:04:21 +0900

    Added the 2nd-order gamma estimation into the LSQ algorithms.

--- fpp_tools.py
+++ fpp_tools.py
@@ -207,10 +207,9 @@ def estimate_phi_N_nonuniform_frames(imagestack, deltas):
 ## ==============================================================================================
 def estimate_phi_lsq(imagestack, deltas):
     '''
-    Using N fringe-projection images, use a least-squares approach to estimate the phase of the underlying object at
-    each pixel in the image. Unlike the conventional algorithm, these fringes need not have uniformly-spaced phase steps.
-    However, the phases ("deltas") need to be known. (This is an alternative but equivalent method to that used by
-    the function "fpp_N_nonuniform_frames()".
+    Using N fringe-projection images with unknown shifts ("deltas"), use a least-squares approach to estimate the
+    phase of the underlying object at each pixel in the image. This method is equivalent to the alternative method
+    "N_nonuniform_frames()", but here is designed to work well with the LSQ deltas-estimator inside an iterative loop.
 
     Parameters
     ----------
@@ -221,8 +220,12 @@ def estimate_phi_lsq(imagestack, deltas):
 
     Returns
     -------
-    phi : 2D array
+    phi_image : 2D array
         The phases (in radians) of the underlying object at each pixel in the images.
+    amplitude_img : 2D array
+        The estimated modulation amplitude at each pixel.
+    bias_image : 2D array
+        The estimated bias value at each pixel.
 
     Notes
     -----
@@ -268,8 +271,8 @@ def estimate_phi_lsq(imagestack, deltas):
 def estimate_deltas_lsq(imagestack, phi_image, nrows=10):
     '''
     Using N fringe-projection images and a guess for the phase "phi" of the underlying object, use a least-squares
-    approach to estimate the phase shifts "deltas" for each of the N images. (This is an complementary algorithm to
-    the function "fpp_estimate_phi()".
+    approach to estimate the phase shifts "deltas" for each of the N images. (This is an complementary algorithm for
+    the function "estimate_phi_lsq()".)
 
     Parameters
     ----------
@@ -340,7 +343,217 @@ def estimate_deltas_lsq(imagestack, phi_image, nrows=10):
     return(deltas)
 
 ## ==============================================================================================
-def estimate_deltas_and_phi_lsq(imagestack, eps=1.0E-3):
+def estimate_phi_lsq2(imagestack, deltas):
+    '''
+    Using N fringe-projection images with unknown shifts ("deltas"), use a least-squares approach to estimate the
+    phase of the underlying object at each pixel in the image. This version of the algorithm simultaneously estimates
+    the nonlinearity parameter "gamma", so that it can perform well even in the presence of nonlinearities.
+
+    Parameters
+    ----------
+    imagestack : list of images
+        The N images to use for estimating the phase phi.
+    deltas : list or array
+        The N phase values (in radians) corresponding to each of the input images.
+
+    Returns
+    -------
+    phi_image : 2D array
+        The phases (in radians) of the underlying object at each pixel in the images.
+    amplitude_img : 2D array
+        The estimated modulation amplitude at each pixel.
+    bias_image : 2D array
+        The estimated bias value at each pixel.
+    gamma_image : 2D array
+        The estimated nonlinearity parameter "gamma" at each pixel.
+
+    Notes
+    -----
+    This algorithm is derived from Z. Wang and B. Han, "Advanced iterative algorithm for phase extraction of
+    randomly phase-shifted interferograms," Optics Letters 29: 1671–1674 (2004).
+    '''
+
+    (Nx,Ny,num_images) = imagestack.shape
+    phi_image = zeros((Nx,Ny), 'float32')
+    A = zeros((5,5), 'float32')
+    B = zeros((5,Ny), 'float32')
+
+    c = cos(deltas)
+    c2 = cos(2.0 * deltas)
+    s = sin(deltas)
+    s2 = sin(2.0 * deltas)
+
+    cs = sum(c * s)
+    cc = sum(c**2)
+    cc2 = sum(c * c2)
+    cs2 = sum(c * s2)
+    ss = sum(s**2)
+    sc2 = sum(s * c2)
+    ss2 = sum(s * s2)
+    c2s2 = sum(c2 * s2)
+
+    A[0,0] = num_images
+    A[0,1] = sum(c)
+    A[0,2] = sum(s)
+    A[0,3] = sum(c2)
+    A[0,4] = sum(s2)
+
+    A[1,0] = A[0,1]
+    A[1,1] = cc
+    A[1,2] = cs
+    A[1,3] = cc2
+    A[1,4] = cs2
+
+    A[2,0] = A[0,2]
+    A[2,1] = cs
+    A[2,2] = ss
+    A[2,3] = sc2
+    A[2,4] = ss2
+
+    A[3,0] = A[0,3]
+    A[3,1] = cc2
+    A[3,2] = sc2
+    A[3,3] = sum(c2**2)
+    A[3,4] = c2s2
+
+    A[4,0] = A[0,4]
+    A[4,1] = cs2
+    A[4,2] = ss2
+    A[4,3] = c2s2
+    A[4,4] = sum(s2**2)
+
+    B0 = sum(imagestack, axis=2)
+    B1 = sum(imagestack * c, axis=2)
+    B2 = sum(imagestack * s, axis=2)
+    B3 = sum(imagestack * c2, axis=2)
+    B4 = sum(imagestack * s2, axis=2)
+    B = dstack([B0, B1, B2, B3, B4])[:,:,:,newaxis]
+
+    X = squeeze(dot(inv(A),B))
+    phi_image = arctan2(-X[2,:], X[1,:])
+    bias_image = X[0,:]
+    amplitude_img = sqrt(X[1,:]**2 + X[2,:]**2)
+
+    coeff_ratio = sqrt(X[3,:]**2 + X[4,:]**2) / amplitude_img
+    gamma_image = (2.0 * coeff_ratio + 1.0) / (1.0 - coeff_ratio)
+
+    return(phi_image, amplitude_img, bias_image, gamma_image)
+
+## ==============================================================================================
+def estimate_deltas_lsq2(imagestack, phi_image, nrows=10):
+    '''
+    Using N fringe-projection images and a guess for the phase "phi" of the underlying object, use a least-squares
+    approach to estimate the phase shifts "deltas" for each of the N images. (This is an complementary algorithm to
+    the function "estimate_phi_lsq()".
+
+    Parameters
+    ----------
+    imagestack : list of images
+        The N images to use for estimating the phase phi.
+    phi_image : ndarray
+        The image giving the phase values (in radians) of the object at each pixel.
+
+    Returns
+    -------
+    deltas : array of float32
+        The phase shift estimates (in radians) for each of the input images in the image stack.
+
+    Notes
+    -----
+    This algorithm is derived from Z. Wang and B. Han, "Advanced iterative algorithm for phase extraction of
+    randomly phase-shifted interferograms," Optics Letters 29: 1671–1674 (2004).
+    '''
+
+    ## If you want to use only a small portion of the image to estimate the deltas, then make "xmax" a small number.
+    ## If you want to use the entire image to estimate the deltas, then use xmax=None
+    (Nx,Ny,num_images) = imagestack.shape
+    Aprime = zeros((5,5), 'float32')
+    Bprime = zeros((5,num_images), 'float32')
+    delta_vectors = zeros((num_images,nrows))
+
+    Aprime[0,0] = Ny
+
+    c = cos(phi_image)
+    c2 = cos(2.0 * phi_image)
+    s = sin(phi_image)
+    s2 = sin(2.0 * phi_image)
+    cs = c * s
+    cc = c**2
+    ss = s**2
+
+    cc2 = c * c2
+    cs2 = c * s2
+    sc2 = s * c2
+    ss2 = s * s2
+    c2s2 = c2 * s2
+
+    Bprime[0,:] = sum(imagestack, axis=(0,1))
+    Bprime[1,:] = sum(imagestack * c[:,:,newaxis], axis=(0,1))
+    Bprime[2,:] = sum(imagestack * s[:,:,newaxis], axis=(0,1))
+    Bprime[3,:] = sum(imagestack * c2[:,:,newaxis], axis=(0,1))
+    Bprime[4,:] = sum(imagestack * s2[:,:,newaxis], axis=(0,1))
+
+    ## Do the estimate separately for each row in the image, for a total of nrows. Then take the average.
+    ## After a good deal of poking around, I still can't find out why the algorithm behaves so much better
+    ## if I keep the Hprime matrix inside the x-loop and update the estimate while traversing few rows,
+    ## rather than getting an Hprime matrix for the entire image. It's only 10 rows, and so it is not slow,
+    ## but not understanding the issue is irritating.
+    for x in range(nrows):
+        cx = sum(c[x,:])
+        c2x = sum(c2[x,:])
+        sx = sum(s[x,:])
+        s2x = sum(s2[x,:])
+        cx_cx = sum(cc[x,:])
+        cx_sx = sum(cs[x,:])
+        sx_sx = sum(ss[x,:])
+
+        cx_c2x = sum(c[x,:] * c2[x,:])
+        cx_s2x = sum(c[x,:] * s2[x,:])
+        sx_c2x = sum(s[x,:] * c2[x,:])
+        sx_s2x = sum(c[x,:] * s2[x,:])
+        c2x_c2x = sum(c2[x,:] * c2[x,:])
+        c2x_s2x = sum(c2[x,:] * s2[x,:])
+        s2x_s2x = sum(s2[x,:] * s2[x,:])
+
+        Aprime[0,1] = cx
+        Aprime[0,2] = sx
+        Aprime[0,3] = c2x
+        Aprime[0,4] = s2x
+
+        Aprime[1,0] = cx
+        Aprime[1,1] = cx_cx
+        Aprime[1,2] = cx_sx
+        Aprime[1,3] = cx_c2x
+        Aprime[1,4] = cx_s2x
+
+        Aprime[2,0] = sx
+        Aprime[2,1] = cx_sx
+        Aprime[2,2] = sx_sx
+        Aprime[2,3] = sx_c2x
+        Aprime[2,4] = sx_s2x
+
+        Aprime[3,0] = c2x
+        Aprime[3,1] = cx_c2x
+        Aprime[3,2] = sx_c2x
+        Aprime[3,3] = c2x_c2x
+        Aprime[3,4] = c2x_s2x
+
+        Aprime[4,0] = s2x
+        Aprime[4,1] = cx_s2x
+        Aprime[4,2] = sx_s2x
+        Aprime[4,3] = c2x_s2x
+        Aprime[4,4] = s2x_s2x
+
+        Xprime = dot(inv(Aprime), Bprime)
+        deltas_estimated = arctan2(-Xprime[2,:], Xprime[1,:])
+        delta_vectors[:,x] = deltas_estimated
+
+    deltas = mean(delta_vectors, axis=1)
+
+    return(deltas)
+
+## ==============================================================================================
+def estimate_deltas_and_phi_lsq(imagestack, eps=1.0E-3, order=1):
     '''
     Using N fringe-projection images, with unknown phase shift values (deltas) and unknown object phase (phi), use an
     iterative least-squares approach to estimate both the delta's and phi's.
@@ -351,6 +564,9 @@ def estimate_deltas_and_phi_lsq(imagestack, eps=1.0E-3):
         The N images to use for estimating the phase phi.
     eps : float
         The mean error value. If the estimated deltas change by less than this amount, we tell the iterative loop to exit.
+    order : int
+        The maximum cosine order to use for estimating coefficients. If the nonlinearity parameter gamma is not equal to 1,
+        then setting order=2 allows one to estimate the phase in the presence of nonlinearity.
 
     Returns
     -------
@@ -365,8 +581,9 @@ def estimate_deltas_and_phi_lsq(imagestack, eps=1.0E-3):
 
     Notes
     -----
-    This algorithm is derived from Z. Wang and B. Han, "Advanced iterative algorithm for phase extraction of
-    randomly phase-shifted interferograms," Optics Letters 29: 1671–1674 (2004).
+    The first-order iterative algorithm used here (sometimes called "AIA") is derived from Z. Wang and B. Han,
+    "Advanced iterative algorithm for phase extraction of randomly phase-shifted interferograms," Optics Letters
+    29: 1671–1674 (2004). The second-order form is something I generalized from their starting point.
     '''
 
     (Nx,Ny,num_images) = imagestack.shape
@@ -379,13 +596,21 @@ def estimate_deltas_and_phi_lsq(imagestack, eps=1.0E-3):
 
     for k in range(niter):
         deltas_new = array(deltas)
-        (phi_image, contrast_image, bias_image) = estimate_phi_lsq(imagestack, deltas_new)
-        deltas = estimate_deltas_lsq(imagestack, phi_image)
+
+        if (order == 1):
+            (phi_img, amplitude_img, bias_img) = estimate_phi_lsq(imagestack, deltas_new)
+            deltas = estimate_deltas_lsq(imagestack, phi_img)
+        elif (order == 2):
+            (phi_img, amplitude_img, bias_img, gamma_img) = estimate_phi_lsq2(imagestack, deltas_new)
+            deltas = estimate_deltas_lsq2(imagestack, phi_img)
+
         epsilons = array(deltas - deltas_new)
         mean_epsilon = mean(abs(epsilons))
 
         if (mean_epsilon < eps):
             break
 
-    return(phi_image, contrast_image, bias_image, deltas)
-
+    if (order == 1):
+        return(phi_img, amplitude_img, bias_img, deltas)
+    elif (order > 1):
+        return(phi_img, amplitude_img, bias_img, gamma_img, deltas)
